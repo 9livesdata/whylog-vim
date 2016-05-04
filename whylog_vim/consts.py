34d5dcec1659ee97927664fb9c104d8ebb0b3117
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-04 22:16:26 +0200

    Add proxy init

---
+++ whylog_vim/consts.py
@@ -0,0 +1,14 @@
+
+
+class EditorStates():
+    EDITOR_NORMAL = 'editor normal'
+    LOG_READER = 'query'
+    TEACHER = 'teacher menu'
+    ADD_CAUSE = 'add cause'
+    TEACHER_INPUT = 'input window'
+    EFFECT_ADDED = 'added effect'
+
+
+class ActionTypes():
+    STANDARD = 'standard action'
+    TEACHER = 'teacher action'
